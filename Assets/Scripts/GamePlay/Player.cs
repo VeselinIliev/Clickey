bc0276c4848cb79b766923ce910686fdfbafc50d
VeselinIliev <Veselin.Iliev@infinno.eu>
2020-03-09 15:17:36 +0200

    added stats panel

--- Assets/Scripts/GamePlay/Player.cs
+++ Assets/Scripts/GamePlay/Player.cs
@@ -5,10 +5,69 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private int level = 1;
+    private int experience = 0;
     private int strength = 10;
     private int agility = 10;
     private int intellingece = 10;
     private int command = 0;
+
+    public int Level
+    {
+        get => level;
+        set { level = value;
+            PlayerPrefs.SetInt("level", value);
+        }
+    }
+
+    public int Experience
+    {
+        get => experience;
+        set
+        { 
+            experience = value;
+            PlayerPrefs.SetInt("exp", value);
+        }
+    }
+
+    public int Strength
+    {
+        get => strength;
+        set
+        {
+            strength = value;
+            PlayerPrefs.SetInt("str", value);
+        }
+    }
+
+    public int Agility
+    {
+        get => agility;
+        set
+        {
+            agility = value;
+            PlayerPrefs.SetInt("agi", value);
+        }
+    }
+
+    public int Intellingece
+    {
+        get => intellingece;
+        set
+        {
+            intellingece = value;
+            PlayerPrefs.SetInt("int", value);
+        }
+    }
+
+    public int Command
+    {
+        get => command;
+        set
+        {
+            command = value;
+            PlayerPrefs.SetInt("comm", value);
+        }
+    }
 
     private void Awake()
     {
@@ -26,7 +85,8 @@ public class Player : MonoBehaviour
     private void UpdateStats()
     {
         level = PlayerPrefs.GetInt("level");
-        strength = PlayerPrefs.GetInt("str");
+        experience = PlayerPrefs.GetInt("exp");
+        Strength = PlayerPrefs.GetInt("str");
         agility = PlayerPrefs.GetInt("agi");
         intellingece = PlayerPrefs.GetInt("int");
         command = PlayerPrefs.GetInt("comm");
@@ -39,31 +99,44 @@ public class Player : MonoBehaviour
         PlayerPrefs.SetInt("agi", 10);
         PlayerPrefs.SetInt("int", 10);
         PlayerPrefs.SetInt("comm", 0);
+        PlayerPrefs.SetInt("exp",0);
         PlayerPrefs.Save();
     }
 
 
-    public static List<int> GetStats()
+    public static List<int> GetStats(Player player)
     {
         List<int> stats = new List<int>();
-
+        stats.Add(player.Level);
+        stats.Add(player.Experience);
+        stats.Add(player.Strength);
+        stats.Add(player.Agility);
+        stats.Add(player.Intellingece);
+        stats.Add(player.command);
+        
         return stats;
+    }
+
+    public List<int> GetCurrentPlayerStats()
+    {
+       return GetStats(this);
     }
 
     public void IncrementStr()
     {
-        strength++;
+        Strength++;
     }
 
     public void IncrementAgi()
     {
-        agility++;
+        Agility++;
     }
 
     public void IncrementInt()
     {
-        intellingece++;
+        Intellingece++;
     }
+
 
 
 }
