6cc2babd6517896b5720781aa15a1332af8596c2
VeselinIliev <Veselin.Iliev@infinno.eu>
2020-03-09 13:02:57 +0200

    project initialised

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    Button start;
+
+    private void Start()
+    {
+        start = GameObject.Find("Start").GetComponent<Button>();
+
+        start.onClick.AddListener(StartGame);
+     
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+
+
+}
