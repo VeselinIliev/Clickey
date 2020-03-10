53b09b1cc85264adeb6bef900b7afcda1d56473a
VeselinIliev <Veselin.Iliev@infinno.eu>
2020-03-10 16:43:32 +0200

    added particles scene

---
+++ Assets/Scripts/GamePlay/Attack/Spells/ISpell.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISpell
+{
+    SpellType SpellType { get; set; }
+    void Cast();
+}
+
+public enum SpellType
+{
+    Ice,
+    Fire,
+    Air,
+    Earth,
+    Shadow
+}
---
+++ Assets/Scripts/GamePlay/Attack/Spells/Spell.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Spell : MonoBehaviour, ISpell
+{
+    private SpellType spellType;
+    public SpellType SpellType { get => spellType; set => spellType = value; }
+
+    public GameObject particleSystemPrefab;
+
+    public void Cast()
+    {
+        GameObject ps = Instantiate(particleSystemPrefab) as GameObject;
+        ps.transform.position = transform.position;
+        ps.GetComponent<ParticleSystem>().Play();
+    }
+
+}
