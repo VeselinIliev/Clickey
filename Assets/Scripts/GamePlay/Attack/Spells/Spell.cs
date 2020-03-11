87ce33d60c225db050a534f06dfde0415f94b056
VeselinIliev <Veselin.Iliev@infinno.eu>
2020-03-11 14:01:59 +0200

    Added frostball and fire ball

--- Assets/Scripts/GamePlay/Attack/Spells/Spell.cs
+++ Assets/Scripts/GamePlay/Attack/Spells/Spell.cs
@@ -7,13 +7,17 @@ public abstract class Spell : MonoBehaviour, ISpell
     private SpellType spellType;
     public SpellType SpellType { get => spellType; set => spellType = value; }
 
+    public int Damage;
+
     public GameObject particleSystemPrefab;
 
     public void Cast()
     {
         GameObject ps = Instantiate(particleSystemPrefab) as GameObject;
+        //var main = ps.GetComponent<ParticleSystem>().main;
         ps.transform.position = transform.position;
         ps.GetComponent<ParticleSystem>().Play();
     }
+
 
 }
