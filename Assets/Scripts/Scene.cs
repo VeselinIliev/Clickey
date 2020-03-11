640e8569d95b2b9952c57c40a5883a9685f2e595
VeselinIliev <Veselin.Iliev@infinno.eu>
2020-03-11 16:46:09 +0200

    particle colors changed and added scene transitions

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    
+    public void LoadFightScene()
+    {
+        SceneManager.LoadScene("FightScene");
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    } public static void LoadFightSceneStatic()
+    {
+        SceneManager.LoadScene("FightScene");
+    }
+
+    public static  void LoadGameSceneStatic()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public static void LoadMainMenuStatic()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+
+
+
+}
